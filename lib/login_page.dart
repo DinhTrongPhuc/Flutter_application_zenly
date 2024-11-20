e24353d51a7d0ac833415437755bdb0be600d71f
DinhTrongPhuc <dnhtrongphuc210104@gmail.com>
2024-11-20 23:26:07 +0700

    20/11/2024 commited

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,145 +1,180 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[100],
-        body: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
           children: [
-            const Text('Hello World, Welcom back!', style: TextStyle(
-              color: Colors.white,
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              ),
-            ),
+            Spacer(),
 
-            const SizedBox(
-              height: 16,
-            ),
-
-            const Text('Login To Continue',
+            const Text(
+              'Hello World, Welcom back!',
               style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Username', 
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.5),
+                color: Colors.white,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
               ),
             ),
 
             const SizedBox(
               height: 16,
-            ),
-
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Password', 
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.5),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(onPressed: () {
-                print('Forgot to Clicked');
-              },
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
-                ),
-                child: const Text('Forgot password?')
-              ),
-            ),
-            SizedBox(
-              width: 250,
-            child: ElevatedButton(onPressed: () {
-                print('Login to Clicked');
-              },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
-                  foregroundColor: Colors.black,
-                ),
-                child: const Text('Login')
-              )
-            ),
-            const SizedBox(
-              height: 50,
             ),
 
             const Text(
+              'Login To Continue',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+
+            Spacer(),
+
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Username',
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.5),
+              ),
+            ),
+
+            const SizedBox(
+              height: 16,
+            ),
+
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                filled: true,
+                fillColor: Colors.white.withOpacity(0.5),
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                  onPressed: () {
+                    print('Forgot to Clicked');
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text('Forgot password?')),
+            ),
+
+            SizedBox(
+              width: 250,
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Login to is Clicked');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black,
+                ),
+                child: const Text('Login'),
+              ),
+            ),
+
+            Spacer(),
+
+            const Text(
               'Or sign in with',
-                style: TextStyle(
+              style: TextStyle(
                 color: Colors.black,
-              ),  
+              ),
             ),
 
             const SizedBox(
               height: 20,
             ),
 
-            ElevatedButton(onPressed: () {
-              print('Google is clicked');
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.black,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(50),
+// google button
+            ElevatedButton(
+              onPressed: () {
+                print('Google is clicked');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
                 ),
               ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/google.png',
+                    width: 25,
+                    height: 25,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text('Login with Google'),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/google.png',
-                  width: 25, 
-                  height: 25,
+
+//facebook button
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
+                  ),
                 ),
-
-                const SizedBox(width: 8,),
-
-                const Text('Login with Google'),
-              ],
-            ),
-          ),
-
-          ElevatedButton(onPressed: () {
-              print('Facebook is clicked');
-            },
-            child: Row(
-              children: [
-                Image.asset(
-                  'assets/images/facebook.png',
-                  width: 25, 
-                  height: 25,
-                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/facebook.png',
+                    width: 25,
+                    height: 25,
+                  ),
                   const Text('Login with FaceBook'),
+                ],
+              ),
+            ),
+
+            Row(
+              children: [
+                const Text("Don't have Account?",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.amber,
+                    ),
+                    child: const Text('Sign Up',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        )))
               ],
             ),
-          ),
-          Row(
-            children: [
-              const Text("Don't have Account?"),
-              TextButton(onPressed: () {}, child: const Text('Sign Up'))
-            ],
-          )
-
-        ],
+            Spacer(),
+          ],
+        ),
       ),
     );
-
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_zenly/login_page.dart';
+import 'package:flutter_application_zenly/styles/app_colors.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Urbanist',
+        scaffoldBackgroundColor: AppColors.background,
+      ),
+      home: const LoginPage(),
     );
   }
 }
